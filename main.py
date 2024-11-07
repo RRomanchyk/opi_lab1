399af042b16a89e642383ec18cf8374281e6dcb3
RRomanchyk <romanbelej235@gmail.com>
2024-11-07 17:29:35 +0200

    easy calculator

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print(":^)")
+ope = input("Введіь оператор  (+ - * /): ")
+
+num1 = int(input("Введіть перше число: "))
+num2 = int(input("Введіть друге число: "))
+
+if ope == "+":
+    print(num1 + num2)
+elif ope == "-":
+    print(num1 - num2)
+elif ope == "*":
+    print(num1 * num2)
+elif ope == "/":
+    print(num1 / num2)
+else:
+    print("Такого оператору не існує")
